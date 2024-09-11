cd0d23381e3e8b03e0cdbb47e1bab55a3a2c9c21
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-09-11 10:11:55 +0700

    convert to functional

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,47 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import Navbar from '../../components/Navbar';
 import SearchBar from './SearchBar';
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
+const Index = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        console.log(process.env.REACT_APP_OMDB_API_KEY);
+        const response = await fetch(`https://www.omdbapi.com/?s=disney&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
+        const movies = await response.json();
+        setMovies(movies.Search);
+      } catch (error) {
+        console.error(error);
+      }
     };
-  }
 
-  async componentDidMount() {
-    try {
-      console.log(process.env.REACT_APP_OMDB_API_KEY);
-      const response = await fetch(`https://www.omdbapi.com/?s=disney&apikey=${process.env.REACT_APP_OMDB_API_KEY}`); //TODO handle possible fetch error
-      const movies = await response.json();
-      this.setState({ movies: movies.Search });
-    }
-    catch (error) {
-      console.error(error);
-    }
-  }
+    fetchMovies();
+  }, []);
 
-
-  render() {
-    return (
-      <div>
-      <Navbar child={<SearchBar/>} />
+  return (
+    <div>
+      <Navbar child={<SearchBar />} />
 
       <div className='container'>
         <h1 className='my-4'>Watch your favorite movies 🍿</h1>
         <div className='row row-cols-2 row-cols-lg-3 row-cols-xl-4 g-2 g-lg-3'>
-          {this.state.movies.map((movie, i) => (
+          {movies.map((movie, i) => (
             <div key={i} className=''>
               <MovieCard movie={movie} />
             </div>
           ))}
         </div>
       </div>
-          </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Index;
