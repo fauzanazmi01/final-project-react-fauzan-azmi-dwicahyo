9b03b1af13fb9305923100bbfe91fb8fe0fa7bcb
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-09-12 09:04:25 +0700

    handle non-existent poster

--- src/pages/Index/MovieCard.jsx
+++ src/pages/Index/MovieCard.jsx
@@ -19,7 +19,7 @@ function truncate(str, n) {
 const MovieCard = ({ movie  }) => {
     return (
         <Link to={movie.imdbID} className="card text-decoration-none bg-body-secondary">
-            <img className="card-img-top" src={movie.Poster} style={{ height: '300px' }}></img>
+            {movie.Poster != 'N/A' ? <img className="card-img-top" src={movie.Poster} style={{ height: '300px' }}></img> : <div className="d-flex justify-content-center align-items-center bg-secondary" style={{ height: '300px' }}>No Image</div>}
             <div className="card-body" style={{ height: '7em' }}>
                 <h5 className="card-title">{truncate(movie.Title, 55)}</h5>
             </div>
